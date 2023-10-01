6962779d1bc99c35be3c7daff59c7d3d1888a464
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-10-01 14:28:23 -0300

    Elimino algunos archivos

--- models/Category.js
+++
@@ -1,9 +0,0 @@
-const { mongoose } = require("../db");
-
-const categorySchema = new mongoose.Schema({
-  name: String,
-});
-
-categorySchema.set("toJSON", { virtuals: true });
-
-module.exports = mongoose.model("Category", categorySchema);
