dd1d5a4929a910203287008ce78e09f6ccf31623
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-10-01 14:20:51 -0300

    Arreglos en server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
+const path = require("path");
 const methodOverride = require("method-override");
 const express = require("express");
-
+const cors = require("cors");
 const routes = require("./routes");
 
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 
 app.use(methodOverride("_method"));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 routes(app);
 
@@ -18,7 +21,6 @@ app.listen(APP_PORT, () => {
   console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
 });
 
-// Esto se ejecuta cuando se "apaga" la app.
 process.on("SIGINT", function () {
   const { mongoose } = require("./db");
   mongoose.connection.close(function () {
