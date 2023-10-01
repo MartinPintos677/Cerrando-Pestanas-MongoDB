d01f4899f9e43624ac6810eebfe50ebf2b2493b7
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-10-01 14:12:24 -0300

    Eliminando cosas

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-//app.set("view engine", "ejs");
 
 routes(app);
 
